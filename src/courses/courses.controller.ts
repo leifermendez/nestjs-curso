6b5e04327322d6f1a1ce18de2e88116e4f6c740b
Leifer Mendez <leifer33@gmail.com>
2022-06-23 13:47:21 +0200

    antes de probar

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -3,11 +3,11 @@ import { CoursesService } from './courses.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 
-@Controller('courses')
+@Controller('courses') //TODO http://localhost:3002/courses
 export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
-  @Post()
+  @Post() //TODO HTTP POST http://localhost:3002/courses
   create(@Body() createCourseDto: CreateCourseDto) {
     return this.coursesService.create(createCourseDto);
   }
@@ -17,17 +17,17 @@ export class CoursesController {
     return this.coursesService.findAll();
   }
 
-  @Get(':id')
+  @Get(':id') //TODO HTTP GET http://localhost:3002/courses/:id
   findOne(@Param('id') id: string) {
     return this.coursesService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch(':id')//TODO HTTP PATCH http://localhost:3002/courses/:id
   update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
     return this.coursesService.update(+id, updateCourseDto);
   }
 
-  @Delete(':id')
+  @Delete(':id') //TODO HTTP DELETE http://localhost:3002/courses/:id
   remove(@Param('id') id: string) {
     return this.coursesService.remove(+id);
   }
